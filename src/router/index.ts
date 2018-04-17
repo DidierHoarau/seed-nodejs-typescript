6e44a26c9e6d2b986bce8dcd34de71e4d8db0cf1
Didier Hoarau <didier.hoarau.dev@gmail.com>
2018-04-17 08:00:18 +0800

    Minor formating fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { exampleGetRouter } from './example-get';
 import { examplePostRouter } from './example-post';
 import { ExpressWrapper } from './utils/express-wrapper';
 
-export let router = ExpressWrapper.createRouter();
+export const router = ExpressWrapper.createRouter();
 
 router.use(`${config.API_PATH}/example/`, exampleGetRouter);
 router.use(`${config.API_PATH}/example/`, examplePostRouter);
